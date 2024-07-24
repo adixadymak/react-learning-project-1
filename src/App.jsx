2ba5f65cb86ea54f6f3487e7891899bf4159abf0
adixadymak <adixadymak@gmail.com>
2024-07-24 12:28:53 +0100

    Created a components folder and extract Header component to a seperate file.

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,6 @@
-import reactImg from "./assets/react-core-concepts.png";
 import { CORE_CONCEPTS } from "./data.js";
+import Header from "./components/Header.jsx";
 
-const reactDescriptions = ["Fundamental", "Crucial", "Core"];
-
-function genRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-function Header() {
-  const description = reactDescriptions[genRandomInt(2)];
-
-  return (
-    <header>
-      <img src={reactImg} alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {description} React concepts you will need for almost any app you are
-        going to build!
-      </p>
-    </header>
-  );
-}
 //You can set the parameters for CoreConcept to 'props' and then extract each props data as 'props.image' etc.
 function CoreConcept({ image, title, description }) {
   return (
