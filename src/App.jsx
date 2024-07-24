6c3037101e3010e2920ae733568bf3e1b6a9959d
adixadymak <adixadymak@gmail.com>
2024-07-24 12:16:28 +0100

    Alternative props syntax 2 - shortcut

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ function Header() {
     </header>
   );
 }
-//You can set the parameters for CoreConcept to 'props' and then extract each props data as 'props.image' etc
+//You can set the parameters for CoreConcept to 'props' and then extract each props data as 'props.image' etc.
 function CoreConcept({ image, title, description }) {
   return (
     <li>
@@ -31,7 +31,7 @@ function CoreConcept({ image, title, description }) {
     </li>
   );
 }
-// But this kind of props syntax is alot of code
+// But when the props name is the same as the data in the object for instance. Use this shortcut.
 function App() {
   return (
     <div>
@@ -45,24 +45,9 @@ function App() {
               title={CORE_CONCEPTS[0].title}
               description={CORE_CONCEPTS[0].description}
             />
-            <CoreConcept
-              image={CORE_CONCEPTS[1].image}
-              title={CORE_CONCEPTS[1].title}
-              description={CORE_CONCEPTS[1].description}
-            />
-            <CoreConcept
-              image={CORE_CONCEPTS[2].image}
-              title={CORE_CONCEPTS[2].title}
-              description={CORE_CONCEPTS[2].description}
-            />
-            <CoreConcept
-              image={CORE_CONCEPTS[3].image}
-              title={CORE_CONCEPTS[3].title}
-              description={CORE_CONCEPTS[3].description}
-            />
-            {/* <CoreConcept />
-            <CoreConcept />
-            <CoreConcept /> */}
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
           </ul>
         </section>
       </main>
