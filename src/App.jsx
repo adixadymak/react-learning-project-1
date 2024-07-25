6cdd21f2a9e06b59230baa033b0c4ccbcedc9f3c
adixadymak <adixadymak@gmail.com>
2024-07-26 00:56:32 +0100

    Working with Fragments - 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, Fragment } from "react";
+import { useState } from "react";
 import { CORE_CONCEPTS } from "./data.js";
 import Header from "./components/Header/Header.jsx";
 import CoreConcept from "./components/CoreConcept.jsx";
@@ -28,7 +28,7 @@ function App() {
   }
 
   return (
-    <Fragment>
+    <>
       <Header />
       <main>
         <section id="core-concepts">
@@ -70,7 +70,7 @@ function App() {
           {tabContent}
         </section>
       </main>
-    </Fragment>
+    </>
   );
 }
 
