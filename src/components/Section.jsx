63698915d068e657b2ca6202be7a0397bd756925
adixadymak <adixadymak@gmail.com>
2024-07-26 02:08:37 +0100

    Forwarding Props To Wrapped Elements

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,9 +1,8 @@
-// Problem: Props Are Not Forwarded To Inner Elements
 //Problem occur if you will need to set many props
 
-export default function Section({ title, id, children }) {
+export default function Section({ title, children, ...props }) {
   return (
-    <section id={id}>
+    <section {...props}>
       <h2>{title}</h2>
       {children}
     </section>
